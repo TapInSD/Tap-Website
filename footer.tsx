227ea1492abd5d645e201c40ab28a1c0c6e5f977
TapInSD <tyrone@taplive.online>
2025-07-17 17:32:20 -0700

    Delete footer.tsx

--- footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-black text-white py-6 text-center">
-      <p>© {new Date().getFullYear()} The Artist Playground (TAP). All rights reserved.</p>
-    </footer>
-  );
-}
